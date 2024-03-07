c21e74ab29083fe217f7c9aa54579957c33ab589
Ryan Cavalcanti <contatodevryan@gmail.com>
2024-03-07 11:47:54 -0300

    Scope rule testing

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 let idade = Number(prompt("Insira sua idade"))
 
-if(idade <= 10){
+if (idade <= 10) {
+    console.log(idade)
+    let dentroEscopo = "Ryan"
+    console.log(dentroEscopo)
     console.log("Você tem menos ou 10 anos")
-} else(
+    if (true) {
+        console.log("Estou no 3º ESCOPO")
+        console.log(dentroEscopo)
+    }
+} else (
     console.log("Você tem mais de 10 anos")
 )
+console.log("Saí fora do escopo do IF")
+console.log(dentroEscopo)
